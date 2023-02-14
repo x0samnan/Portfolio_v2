f77e397fe3305b994a42f916d7de323e35ed59f5
Samnan <samnansiddique@yandex.com>
2023-02-14 21:21:37 +0530

    Header modified

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,14 +5,14 @@ import { IoMdMoon } from 'react-icons/io'
 const header = ({darkMode, setDarkMode}) => {
   return (
     <header className='bg-white dark:bg-gray-900
-     text-gray-900 dark:text-white'>
+     text-gray-900'>
       <nav className='flex justify-between items-center p-5'>
-        <h1 className='text-4xl'>
+        <h1 className='text-4xl dark:text-green-500'>
           SAMNAN
         </h1>
         <div onClick={() => setDarkMode(!darkMode)}>
           {
-            darkMode ? <MdWbSunny className='text-2xl cursor-pointer'/> 
+            darkMode ? <MdWbSunny className='text-2xl cursor-pointer dark:text-white'/> 
             : <IoMdMoon className='text-2xl cursor-pointer'/>
           }
         </div>
