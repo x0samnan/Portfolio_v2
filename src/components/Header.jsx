191071e95ab61d78d53ff2039c7aceb5f9ec1c49
Samnan <samnansiddique@yandex.com>
2023-02-15 08:47:22 +0530

    Typewriter added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import { MdWbSunny } from 'react-icons/md'
 import { IoMdMoon } from 'react-icons/io'
+import { Cursor, useTypewriter } from 'react-simple-typewriter'
 
-const header = ({darkMode, setDarkMode}) => {
+const Header = ({darkMode, setDarkMode}) => {
+  const [text] = useTypewriter ({
+    words: [
+      `root@samnan`, 
+      `Fetching details...`,
+      `An undergrad student`,
+      `A developer`, 
+      `An open-source enthusiast`,
+      `And a cold coffee lover`,
+      ],
+    loop: true,
+    delaySpeed: 1750,
+});
+
   return (
     <header className='bg-white dark:bg-gray-900
      text-gray-900'>
       <nav className='flex justify-between items-center p-5'>
-        <h1 className='text-4xl dark:text-green-500'>
-          SAMNAN
+        <h1 className='text-lg dark:text-green-500'>
+          <span> (->) {text}</span>
+          <Cursor cursorColor='#0FFF50' />
         </h1>
         <div onClick={() => setDarkMode(!darkMode)}>
           {
@@ -21,4 +36,4 @@ const header = ({darkMode, setDarkMode}) => {
   )
 }
 
-export default header
+export default Header
