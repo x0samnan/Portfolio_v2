551337d56c6df265e007dfd704593d136087b1f6
Samnan <samnansiddique@yandex.com>
2023-03-06 20:26:11 +0530

    Scale changed on hover

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -51,10 +51,10 @@ const Projects = () => {
                 <img src={image} alt={title} className='w-2/3'/>
                 <div className='w-1/3 flex flex-col items-center justify-evenly p-1'>
                     <h2>{title}</h2>
-                    <a className='text-3xl cursor-pointer duration-150 hover:scale-125 hover:text-green-500' href={github}
+                    <a className='text-2xl cursor-pointer duration-150 hover:scale-125 hover:text-green-500' href={github}
                     target='_blank'
                     rel='noopener noreferrer'><FaGithub /></a>
-                    <a className='text-3xl cursor-pointer duration-150 hover:scale-125 hover:text-green-500' href={Deployment}
+                    <a className='text-2xl cursor-pointer duration-150 hover:scale-125 hover:text-green-500' href={Deployment}
                     target='_blank'
                     rel='noopener noreferrer'><FaExternalLinkSquareAlt /></a>
                 </div>
